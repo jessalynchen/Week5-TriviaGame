b3e077150d41cc29a1bfbe64848c116782cd846e
jessalynchen <jessalynchen@gmail.com>
2018-10-04 20:59:16 -0400

    fix score functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
 console.log("The js file is linked!");
-
-<<<<<<< HEAD
 //once you click submit the total score shows
-var totalScore = 0;
 
+var totalScore = 0;
 
 window.onload = function () {
     var fiveMinutes = 60 * 5,
@@ -29,58 +27,24 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-
-$(document).ready(function(){
-
-=======
-$(document).ready(function(){
-
-    console.log("Page is ready");
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
     
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
->>>>>>> parent of 897c733... instructions
-    }
-    
-
-    //when a radio button is clicked then add the button into an array
-
-
-    $("#submit").click(function(){
-        console.log("Submit button is clicked");
-        console.log(userAnswer);
-        $("#score").text(totalScore);
-        //write a check answers function 
-
-    });
-
-    //once you click submit the total score shows
-    var totalScore = 0;
-    $("#score").text(totalScore);
-//check answers after submit 
+    //when a radio button is clicked, check if correct value
 $(function() {
    console.log(sessionStorage.getItem('option'));
     $('[type=radio]').click(function() {
-             console.log(this.value);           sessionStorage.setItem('option',this.value);
-        userAnswer.push(this.value);
+             console.log(this.value);  ;
+            
         if (this.value === "correct"){
             totalScore +=1;
+            
         } 
         });    
 
-});
-
-
+//show correct answer score after submit 
+$("#submit").click(function(){
+    console.log("Submit button is clicked");   
+    
+    $("#score").text(totalScore);
     });
+
+});
