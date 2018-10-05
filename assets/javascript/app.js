18c413fb3bdfa34e915ed3e314a4b8ed2cff61c8
jessalynchen <jessalynchen@gmail.com>
2018-10-04 20:18:30 -0400

    timer update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,41 @@
 console.log("The js file is linked!");
-var userAnswer = [];
 
 //once you click submit the total score shows
 var totalScore = 0;
 
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+
+console.log("Page is ready");
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
 $(document).ready(function(){
 
 
-    console.log("Page is ready");
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
+    if (timer === 0){
+        $("#score").text(totalScore);
     }
     
-    window.onload = function () {
-        var fiveMinutes = 60 * 5,
-            display = document.querySelector('#time');
-        startTimer(fiveMinutes, display);
-    };
 
     //when a radio button is clicked then add the button into an array
 
