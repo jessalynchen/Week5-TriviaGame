24fd298821d2a5e569b27f390d80dc157385b1f8
jessalynchen <jessalynchen@gmail.com>
2018-10-04 20:35:49 -0400

    revert changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 console.log("The js file is linked!");
 
+<<<<<<< HEAD
 //once you click submit the total score shows
 var totalScore = 0;
 
@@ -34,6 +35,25 @@ $(document).ready(function(){
 
     if (timer === 0){
          $("#score").text(totalScore);
+
+$(document).ready(function(){
+
+    console.log("Page is ready");
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
     }
     
 
@@ -42,16 +62,21 @@ $(document).ready(function(){
 
     $("#submit").click(function(){
         console.log("Submit button is clicked");
+        
         console.log(userAnswer);
         $("#score").text(totalScore);
         //write a check answers function 
 
     });
-   
+
+    //once you click submit the total score shows
+    var totalScore = 0;
+    $("#score").text(totalScore);
 //check answers after submit 
 $(function() {
+   console.log(sessionStorage.getItem('option'));
     $('[type=radio]').click(function() {
-             console.log(this.value);           
+             console.log(this.value);           sessionStorage.setItem('option',this.value);
         userAnswer.push(this.value);
         if (this.value === "correct"){
             totalScore +=1;
@@ -59,6 +84,6 @@ $(function() {
         });    
 
 });
-
+}
 
     });
